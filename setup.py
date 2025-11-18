fdb6af353e0db1731149a88d5f232aa865fb8236
vv3-git <vishal3srkt@gmail.com>
2025-11-19 04:18:37 +0530

    version update

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except FileNotFoundError:
 
 setup(
     name="snowball",      
-    version="1.3.9",                            # Package version
+    version="1.4.1",                            # Package version
     description="Generate dbt , sql projects and PySpark notebooks.",
     long_description=long_description,          # Long description (usually README content)
     long_description_content_type="text/markdown",
@@ -33,7 +33,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'snowball = snowball:run_dbt.main',
+            'snowball = snowball:snowball.main',
         ],
     },
     classifiers=[                               
